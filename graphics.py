21f0dd04b39786bfd7b4ce2759e72f6e6c69489a
fronceee <tharathip.tamwiset@gmail.com>
2024-06-22 23:53:27 +0700

    init project

---
+++ graphics.py
@@ -0,0 +1,44 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        
+        self.__root = Tk()
+        self.__root.title("Maze Solver") 
+        
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=True)
+        
+        self.is_running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+        
+    def wait_for_close(self):
+        self.is_running = True
+        while self.is_running:
+            self.redraw()
+            
+    def close(self):
+        self.is_running = False
+        
+    def draw_line(self, line, fill_color = "black"):
+        line.draw(self.__canvas, fill_color)
+        
+class Point():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        
+class Line():
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        
+    def draw(self, canvas, fill_color = "black"):
+        canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color)
+
